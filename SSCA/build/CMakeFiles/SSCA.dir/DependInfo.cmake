e5c9d95c5aa7fbc8cf42718fd51d5083020210ce
foowaa <tianchunlin123@gmail.com>
2018-03-16 16:06:04 +0800

    init and add SSCA

---
+++ SSCA/build/CMakeFiles/SSCA.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/nvidia/project/2/SSCA/CAST/ctmf1.c" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/CAST/ctmf1.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nvidia/project/2/SSCA/CAFilter/BFCA.cpp" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/CAFilter/BFCA.cpp.o"
+  "/home/nvidia/project/2/SSCA/CAFilter/BilateralFilter.cpp" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/CAFilter/BilateralFilter.cpp.o"
+  "/home/nvidia/project/2/SSCA/CAFilter/BoxCA.cpp" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/CAFilter/BoxCA.cpp.o"
+  "/home/nvidia/project/2/SSCA/CAFilter/GFCA.cpp" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/CAFilter/GFCA.cpp.o"
+  "/home/nvidia/project/2/SSCA/CAFilter/GuidedFilter.cpp" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/CAFilter/GuidedFilter.cpp.o"
+  "/home/nvidia/project/2/SSCA/CAST/STCA.cpp" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/CAST/STCA.cpp.o"
+  "/home/nvidia/project/2/SSCA/CAST/SegmentTree.cpp" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/CAST/SegmentTree.cpp.o"
+  "/home/nvidia/project/2/SSCA/CAST/StereoDisparity.cpp" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/CAST/StereoDisparity.cpp.o"
+  "/home/nvidia/project/2/SSCA/CAST/StereoHelper.cpp" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/CAST/StereoHelper.cpp.o"
+  "/home/nvidia/project/2/SSCA/CAST/Toolkit.cpp" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/CAST/Toolkit.cpp.o"
+  "/home/nvidia/project/2/SSCA/CC/CGCC.cpp" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/CC/CGCC.cpp.o"
+  "/home/nvidia/project/2/SSCA/CC/CenCC.cpp" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/CC/CenCC.cpp.o"
+  "/home/nvidia/project/2/SSCA/CC/GrdCC.cpp" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/CC/GrdCC.cpp.o"
+  "/home/nvidia/project/2/SSCA/PPSG/SGPP.cpp" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/PPSG/SGPP.cpp.o"
+  "/home/nvidia/project/2/SSCA/PPWM/WMPP.cpp" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/PPWM/WMPP.cpp.o"
+  "/home/nvidia/project/2/SSCA/SSCA.cpp" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/SSCA.cpp.o"
+  "/home/nvidia/project/2/SSCA/cvtools.cpp" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/cvtools.cpp.o"
+  "/home/nvidia/project/2/SSCA/main1.cpp" "/home/nvidia/project/2/SSCA/build/CMakeFiles/SSCA.dir/main1.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../CAFilter"
+  "../CANLC"
+  "../CAST"
+  "../CC"
+  "../PPSG"
+  "../PPWM"
+  "/usr/include/opencv"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ SSCA/main1.cpp
@@ -0,0 +1,169 @@
+///////////////////////////////////////////////////////
+// File: main.cpp
+// Desc: Scale Space Cost Aggregation
+// Usage: [CC_METHOD] [CA_METHOD] [USE_MED] [lImg] [rImg] [lDis] [maxDis] [disSc]
+// Author: Zhang Kang
+// Date: 2013/09/06
+///////////////////////////////////////////////////////
+#include "CommFunc.h"
+#include "SSCA.h"
+#include "CC/GrdCC.h"
+#include "CAFilter/GFCA.h"
+#include "GetMehod.h"
+#include "cvtools.h"
+
+#define USE_MEDIAN_FILTER
+
+#ifdef USE_MEDIAN_FILTER
+#include"CAST/Toolkit.h"
+#endif
+
+int main( int argc, char** argv )
+{
+	printf( "Scale Space Cost Aggregation\n" );
+	/*if( argc != 10 ) {
+		printf( "Usage: [CC_METHOD] [CA_METHOD] [PP_METHOD] [C_ALPHA] [lImg] [rImg] [lDis] [maxDis] [disSc]\n" );
+		printf( "\nPress any key to continue...\n" );
+		getchar();
+		//return -1;
+	}*/
+	// ccName GRD CEN CG 
+	// GRD--intensity+gradient; CEN--Census Transform; CG--Census+gradient;
+	string ccName = "GRD";
+	// caName GF BF BOX NL 
+	// GF--guided image filter; BF--bilateral filter; BOX--box filter; NL--non-local cost aggregation;  
+	string caName = "GF";
+	// ppName WN--weighted median filtering
+	string ppName = "WM";
+	double costAlpha = atof( "0.3" );
+	//string lFn = "G:/StereoMatching/Rectified/left_38.bmp";
+	
+	
+	//for (int s = 56; s < 77; s++) {
+		//ostringstream sStr;
+		//sStr << s;
+		//string lFn = "G:/StereoMatching/kitti2/L/00000000" + sStr.str() + ".png";
+		//string rFn = "G:/StereoMatching/kitti2/R/00000000" + sStr.str() + ".png";
+		//string lDisFn = "G:/StereoMatching/kitti2/dis/00000000" + sStr.str() + ".png";
+	string lFn = "/home/nvidia/project/2/SSCA/data/L.png";
+	string rFn = "/home/nvidia/project/2/SSCA/data/R.png";
+	string lDisFn = "/home/nvidia/project/2/SSCA/data/dis0001.jpg";
+
+		int maxDis = atoi("256");
+		int disSc = atoi("1");
+		//
+		// Load left right image
+		//
+		printf("\n--------------------------------------------------------\n");
+		//printf( "Load Image: (%s) (%s)\n");
+		printf("--------------------------------------------------------\n");
+		Mat lImg = imread(lFn, CV_LOAD_IMAGE_COLOR);
+		//cv::imshow("lImg", lImg);
+		Mat rImg = imread(rFn, CV_LOAD_IMAGE_COLOR);
+		if (!lImg.data || !rImg.data) {
+			printf("Error: can not open image\n");
+			printf("\nPress any key to continue...\n");
+			getchar();
+			return -1;
+		}
+		// set image format
+		cvtColor(lImg, lImg, CV_BGR2RGB);
+		cvtColor(rImg, rImg, CV_BGR2RGB);
+		lImg.convertTo(lImg, CV_64F, 1 / 255.0f);
+		rImg.convertTo(rImg, CV_64F, 1 / 255.0f);
+
+		// time
+		double duration;
+		duration = static_cast<double>(getTickCount());
+
+		//
+		// Stereo Match at each pyramid
+		//
+		int PY_LVL = 5;
+		// build pyramid and cost volume
+		Mat lP = lImg.clone();
+		Mat rP = rImg.clone();
+		SSCA** smPyr = new SSCA*[PY_LVL];
+		CCMethod* ccMtd = getCCType(ccName);
+		CAMethod* caMtd = getCAType(caName);
+		PPMethod* ppMtd = getPPType(ppName);
+		for (int p = 0; p < PY_LVL; p++) {
+			if (maxDis < 5) {
+				PY_LVL = p;
+				break;
+			}
+			printf("\n\tPyramid: %d:", p);
+			smPyr[p] = new SSCA(lP, rP, maxDis, disSc);
+
+
+			smPyr[p]->CostCompute(ccMtd);
+
+			//if( p== 0 ) {
+			//		smPyr[ 0 ]->saveCostVol( "CCGRD.txt" );
+			//}
+			smPyr[p]->CostAggre(caMtd);
+			// pyramid downsample
+			maxDis = maxDis / 2 + 1;
+			disSc *= 2;
+			pyrDown(lP, lP);
+			pyrDown(rP, rP);
+		}
+		printf("\n--------------------------------------------------------\n");
+		printf("\n Cost Aggregation in Scale Space\n");
+		printf("\n--------------------------------------------------------\n");
+		// new method
+		SolveAll(smPyr, PY_LVL, costAlpha);
+
+		// old method
+		//for( int p = PY_LVL - 2 ; p >= 0; p -- ) {
+		//	smPyr[ p ]->AddPyrCostVol( smPyr[ p + 1 ], costAlpha );
+		//}
+
+		//
+		// Match + Postprocess
+		//
+		smPyr[0]->Match();
+		smPyr[0]->PostProcess(ppMtd);
+		Mat lDis = smPyr[0]->getLDis();
+
+#ifdef USE_MEDIAN_FILTER
+		//
+		// Median Filter Output
+		//
+		MeanFilter(lDis, lDis, 3);
+		printf("use median filter");
+#endif
+		duration = static_cast<double>(getTickCount()) - duration;
+		duration /= cv::getTickFrequency(); // the elapsed time in sec
+		printf("\n--------------------------------------------------------\n");
+		printf("Total Time: %.2lf s\n", duration);
+		printf("--------------------------------------------------------\n");
+
+		//
+		// Save Output
+		//
+		//cvtools::writeMat(lDis, "lDis.mat", "lDis");
+
+		imwrite(lDisFn, lDis);
+		//imwrite( lDisFn1, lDis);
+
+		//
+		// For drawing - save cost volume
+		//
+		string costFn = "";
+		if (costAlpha > 0.0) {
+			costFn = "S_" + caName + ".txt";
+		}
+		else {
+			costFn = caName + ".txt";
+		}
+
+		smPyr[0]->saveCostVol(costFn.c_str());
+
+		delete[] smPyr;
+		delete ccMtd;
+		delete caMtd;
+		delete ppMtd;
+		return 0;
+}
+
